31753d50234ea43b99101a30fb54712291e73fda
Richard Remer <rich.remer@gmail.com>
2025-10-13 16:47:26 -0700

    test EFI StdErr interface

--- bootx64.efi/efi_main.c
+++ bootx64.efi/efi_main.c
@@ -2,7 +2,13 @@
 #include <efilib.h>
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"booting custom EFI\n");
+    SIMPLE_TEXT_OUTPUT_INTERFACE* out = SystemTable->ConOut;
+    SIMPLE_TEXT_OUTPUT_INTERFACE* err = SystemTable->StdErr;
+
+    out->OutputString(out, L"booting custom EFI\n");
+    err->OutputString(err, L"bootloader does nothing\n");
+
     for (;;) ;
+
     return EFI_SUCCESS;
 }
