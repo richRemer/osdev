4d7f1c59f41f46bb5e707b6259b1bfb583d079c6
Richard Remer <rich.remer@gmail.com>
2025-10-15 03:34:19 -0700

    memory map

--- bootx64.efi/efi_main.c
+++ bootx64.efi/efi_main.c
@@ -4,8 +4,11 @@
 
 static CHAR16* Booting = L"Booting custom EFI\r\n";
 static CHAR16* DisplayMode = L"Mode %d ( %q x %q )\r\n";
+static CHAR16* MapLoaded = L"Memory map loaded\r\n";
+static CHAR16* MemDescriptor = L"Memory Descriptor:\r\n";
 
 static void enable_cursor(EFI_SYSTEM_TABLE*);
+static void print_mem_map(EFI_SYSTEM_TABLE*);
 static void print_mode_info(EFI_SYSTEM_TABLE*);
 static void print_modes(EFI_SYSTEM_TABLE*);
 static void wait_forever();
@@ -18,6 +21,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable
 
     print_mode_info(SystemTable);
     print_modes(SystemTable);
+    print_mem_map(SystemTable);
     enable_cursor(SystemTable);
     wait_forever();
 
@@ -28,6 +32,60 @@ static void enable_cursor(EFI_SYSTEM_TABLE* SystemTable) {
     SIMPLE_TEXT_OUTPUT_INTERFACE* out = SystemTable->ConOut;
     
     out->EnableCursor(out, TRUE);
+}
+
+static void print_mem_map(EFI_SYSTEM_TABLE* SystemTable) {
+    SIMPLE_TEXT_OUTPUT_INTERFACE* out = SystemTable->ConOut;
+    UINTN MemoryMapSize = 0;
+    UINTN MapKey = 0;
+    UINTN DescriptorSize = 0;
+    UINT32 DescriptorVersion = 0;
+    EFI_MEMORY_DESCRIPTOR* MemoryMap;
+    EFI_STATUS Status;
+    CHAR16 buf[100];
+
+    Status = SystemTable->BootServices->GetMemoryMap(&MemoryMapSize,
+        NULL, &MapKey, &DescriptorSize, &DescriptorVersion);
+
+    if (Status != EFI_BUFFER_TOO_SMALL) {
+        // can't get buffer size; just bail out for now
+        return;
+    }
+
+    // considered good practice in case map grows between calls
+    MemoryMapSize += 2*DescriptorSize;
+
+    Status = SystemTable->BootServices->AllocatePool(EfiLoaderData,
+        MemoryMapSize, (void**)&MemoryMap);
+
+    if (EFI_ERROR(Status)) {
+        // failed to allocate memory
+        return;
+    }
+
+    Status = SystemTable->BootServices->GetMemoryMap(&MemoryMapSize,
+        MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
+
+    if (EFI_ERROR(Status)) {
+        // failed to load map
+        SystemTable->BootServices->FreePool(MemoryMap);
+        return;
+    }
+
+    out->OutputString(out, MapLoaded);
+    out->OutputString(out, fmt(buf, 100, L"Map Key: %q\r\n", MapKey));
+    out->OutputString(out, fmt(buf, 100, L"Descriptor Size: %q\r\n", DescriptorSize));
+
+    EFI_MEMORY_DESCRIPTOR* MemDesc = MemoryMap;
+
+    for (int i=0; i<MemoryMapSize/DescriptorSize; i++) {
+        out->OutputString(out, MemDescriptor);
+        out->OutputString(out, fmt(buf, 100, L"-Type: %d\r\b", MemDesc->Type));
+        out->OutputString(out, fmt(buf, 100, L"-Physical Start: %Q\r\b", MemDesc->PhysicalStart));
+        out->OutputString(out, fmt(buf, 100, L"-Virtual Start: %Q\r\b", MemDesc->VirtualStart));
+        out->OutputString(out, fmt(buf, 100, L"-Number Of Pages: %q\r\b", MemDesc->NumberOfPages));
+        out->OutputString(out, fmt(buf, 100, L"-Attribute: %q\r\b", MemDesc->Attribute));
+    }
 }
 
 static void print_mode_info(EFI_SYSTEM_TABLE* SystemTable) {
@@ -53,7 +111,7 @@ static void print_modes(EFI_SYSTEM_TABLE* SystemTable) {
 
         out->QueryMode(out, mode, &cols, &rows);
         
-        fmt(buf, 100, L"Mode %d ( %q x %q )\r\n", mode, cols, rows);
+        fmt(buf, 100, L"-Mode %d ( %q x %q )\r\n", mode, cols, rows);
         
         out->OutputString(out, buf);
     }
--- bootx64.efi/efi_string.c
+++ bootx64.efi/efi_string.c
@@ -1,10 +1,15 @@
 #include "efi_string.h"
 
-//static const CHAR16* hexit = L"0123456789ABCDEF";
+static const CHAR16* hexit = L"0123456789ABCDEF";
 static const CHAR16* digit = L"0123456789";
 
+static uint8_t btou(CHAR16*, uint8_t, uint8_t, uint8_t);
+static uint8_t dtou(CHAR16*, uint8_t, uint8_t, uint32_t);
 static uint8_t itou(CHAR16*, uint8_t, uint8_t, int64_t);
+static uint8_t qtou(CHAR16*, uint8_t, uint8_t, uint64_t);
 static uint8_t utou(CHAR16*, uint8_t, uint8_t, uint64_t);
+static uint8_t wtou(CHAR16*, uint8_t, uint8_t, uint16_t);
+static uint8_t xtou(CHAR16*, uint8_t, uint8_t, uint64_t, uint8_t);
 
 CHAR16* fmt(CHAR16* buffer, uint8_t size, const CHAR16* format, ...) {
     int f_idx = 0;
@@ -18,9 +23,13 @@ CHAR16* fmt(CHAR16* buffer, uint8_t size, const CHAR16* format, ...) {
             switch (format[++f_idx]) {
                 case '%': buffer[d_idx++] = '%'; break;
                 case 'b': d_idx = utou(buffer, size, d_idx, va_arg(args, int)); break;
+                case 'B': d_idx = btou(buffer, size, d_idx, va_arg(args, int)); break;
                 case 'w': d_idx = utou(buffer, size, d_idx, va_arg(args, int)); break;
+                case 'W': d_idx = wtou(buffer, size, d_idx, va_arg(args, int)); break;
                 case 'd': d_idx = utou(buffer, size, d_idx, va_arg(args, uint32_t)); break;
+                case 'D': d_idx = dtou(buffer, size, d_idx, va_arg(args, uint32_t)); break;
                 case 'q': d_idx = utou(buffer, size, d_idx, va_arg(args, uint64_t)); break;
+                case 'Q': d_idx = qtou(buffer, size, d_idx, va_arg(args, uint64_t)); break;
                 default:
                     buffer[d_idx++] = '%';
                     buffer[d_idx++] = format[f_idx];
@@ -43,12 +52,24 @@ CHAR16* fmt(CHAR16* buffer, uint8_t size, const CHAR16* format, ...) {
     return buffer;
 }
 
+static uint8_t btou(CHAR16* buffer, uint8_t size, uint8_t offset, uint8_t byte) {
+    return xtou(buffer, size, offset, byte, 1);
+}
+
+static uint8_t dtou(CHAR16* buffer, uint8_t size, uint8_t offset, uint32_t dword) {
+    return xtou(buffer, size, offset, dword, 4);
+}
+
 static uint8_t itou(CHAR16* buffer, uint8_t size, uint8_t offset, int64_t sint) {
     if (offset < size && sint < 0) {
         buffer[offset++] = '-';
     }
 
     return utou(buffer, size, offset, sint < 0 ? -sint : sint);
+}
+
+static uint8_t qtou(CHAR16* buffer, uint8_t size, uint8_t offset, uint64_t qword) {
+    return xtou(buffer, size, offset, qword, 8);
 }
 
 static uint8_t utou(CHAR16* buffer, uint8_t size, uint8_t offset, uint64_t uint) {
@@ -67,6 +88,22 @@ static uint8_t utou(CHAR16* buffer, uint8_t size, uint8_t offset, uint64_t uint)
         while (charidx < 20 && offset < size) {
             buffer[offset++] = chars[charidx++];
         }
+    }
+
+    return offset;
+}
+
+static uint8_t wtou(CHAR16* buffer, uint8_t size, uint8_t offset, uint16_t word) {
+    return xtou(buffer, size, offset, word, 2);
+}
+
+static uint8_t xtou(CHAR16* buffer, uint8_t size, uint8_t offset, uint64_t value, uint8_t octets) {
+    uint8_t byte;
+
+    while (offset < size && octets > 0) {
+        byte = value >> (--octets * 8);
+        buffer[offset++] = hexit[byte >> 4];
+        if (offset < size) buffer[offset++] = hexit[byte & 0xF];
     }
 
     return offset;
