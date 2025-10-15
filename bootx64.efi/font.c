bd75655f17c87fdb72aed4a4454728ae46b65e1d
Richard Remer <rich.remer@gmail.com>
2025-10-15 16:38:29 -0700

    fix positioning of font pagination

--- bootx64.efi/font.c
+++ bootx64.efi/font.c
@@ -29,11 +29,11 @@ static void font_refresh_view(App* app) {
         }
     }
 
-    efi_cursor_to(4, 12);
+    efi_cursor_to(4, 20);
     efi_out_style(EFI_WHITE, L"%B", last_page);
-    efi_cursor_to(25, 12);
+    efi_cursor_to(25, 20);
     efi_out_style(EFI_WHITE, L"%B", page);
-    efi_cursor_to(46, 12);
+    efi_cursor_to(46, 20);
     efi_out_style(EFI_WHITE, L"%B", next_page);
 }
 
