327d36acf2967c971380640ab0e7d5247517b81d
Richard Remer <rich.remer@gmail.com>
2025-10-16 15:15:16 -0700

    work around finicky IDE not seeing header

--- dbfs.linux/dbfs.c
+++ dbfs.linux/dbfs.c
@@ -7,6 +7,11 @@
 #include <errno.h>
 
 static const char* metadata = "{\"name\":\"foo\"}\n";
+static const unsigned int m_umask = 022;
+static const unsigned int m_perm_dir = 0777;
+static const unsigned int m_perm_reg = 0666;
+static const unsigned int m_dir = 040000;
+static const unsigned int m_reg = 0100000;
 
 static int dbfs_getattr(
     const char* path,
@@ -17,15 +22,15 @@ static int dbfs_getattr(
 
     if (0 == strcmp(path, "/")) {
         stat->st_ino = 1;
-        stat->st_mode = S_IFDIR | 0755;
-        stat->st_nlink = 2;
+        stat->st_mode = m_dir | (m_perm_dir & ~m_umask);
+        stat->st_nlink = 1;
         stat->st_uid = 0;
         stat->st_gid = 0;
         stat->st_size = 0;
     } else if (0 == strcmp(path, "/.meta")) {
         const size_t size = strlen(metadata);
         stat->st_ino = 2;
-        stat->st_mode = S_IFREG | 0644;
+        stat->st_mode = m_reg | (m_perm_reg & ~m_umask);
         stat->st_nlink = 1;
         stat->st_uid = 0;
         stat->st_gid = 0;
