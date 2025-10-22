b274874316a3b3164c8a227a166e4bd5f7701773
Richard Remer <rich.remer@gmail.com>
2025-10-21 22:11:34 -0700

    load kernel segments

--- bootz64.efi/src/bootx64.zig
+++ bootz64.efi/src/bootx64.zig
@@ -11,8 +11,10 @@ pub fn main() void {
 }
 
 fn load() !void {
-    var base_address: u64 = 0x100000; // 1 MiB minimum base address
+    var phys_address: u64 = 0x100000; // 1 MiB minimum base address
     var free_pages: u64 = 0;
+    var vaddr_offset: u64 = 0;
+    var kernel_address: u64 = 0;
 
     const allocator = uefi.pool_allocator;
     const boot = uefi.system_table.boot_services.?;
@@ -26,8 +28,8 @@ fn load() !void {
     // find highest physical address in map to place kernel
     var mmap_it = mmap.iterator();
     while (mmap_it.next()) |mem| {
-        if (mem.type == .conventional_memory and mem.physical_start >= base_address) {
-            base_address = mem.physical_start;
+        if (mem.type == .conventional_memory and mem.physical_start >= phys_address) {
+            phys_address = mem.physical_start;
             free_pages = mem.number_of_pages;
         }
     }
@@ -50,11 +52,70 @@ fn load() !void {
     defer allocator.free(prog_headers_buffer);
 
     var ph_it = elf_header.iterateProgramHeadersBuffer(headers_buffer);
-    while (try ph_it.next()) |section| {
-        _ = section;
+    var first_segment = true;
+    while (try ph_it.next()) |segment| {
+        if (segment.p_type == elf.PT_LOAD) {
+            // assume first loaded segment is kernel
+            if (first_segment) {
+                kernel_address = segment.p_vaddr;
+                vaddr_offset = kernel_address - phys_address;
+                first_segment = false;
+            }
+
+            try load_segment(
+                kernel_file,
+                segment.p_offset,
+                segment.p_filesz,
+                segment.p_memsz,
+                segment.p_vaddr - vaddr_offset,
+            );
+        }
+    }
+
+    if (first_segment) {
+        return error.NoCode;
     }
 
     return error.KernelFound;
+}
+
+fn load_segment(
+    /// open ELF file
+    file: *File,
+    /// offset into file where segment begins
+    offset: u64,
+    /// length of segment in file
+    length: usize,
+    /// size of segment in memory
+    size: usize,
+    /// virtual address of segment
+    vaddr: u64,
+) !void {
+    if (vaddr & 0xFFF != 0) return error.SegmentUnaligned;
+    if (length > size) return error.SegmentUnfit;
+
+    const boot = uefi.system_table.boot_services.?;
+    const pages = try boot.allocatePages(
+        .{ .address = @ptrFromInt(vaddr) },
+        .loader_data,
+        size >> 12,
+    );
+    errdefer boot.freePages(pages) catch {};
+
+    var segment: []u8 = &.{};
+
+    segment.ptr = @ptrCast(pages.ptr);
+    segment.len = pages.len * 4096;
+
+    try file.setPosition(offset);
+    _ = try file.read(segment[0..length]);
+
+    const zero_start: u64 = vaddr + length;
+    const zero_count: u64 = size - length;
+
+    if (zero_count > 0) {
+        @memset(@as([*]u8, @ptrFromInt(zero_start))[0..zero_count], 0);
+    }
 }
 
 fn read_elf_header(file: *File) !elf.Header {
