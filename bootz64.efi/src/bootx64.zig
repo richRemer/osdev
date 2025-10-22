dc7b41f4f394e4e2f05489f798f81c365d93167c
Richard Remer <rich.remer@gmail.com>
2025-10-21 18:04:02 -0700

    simple attempt to load a kernel image from disk

--- bootz64.efi/src/bootx64.zig
+++ bootz64.efi/src/bootx64.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
+const efi = @import("efi.zig");
+const unicode = std.unicode;
+const uefi = std.os.uefi;
+const SimpleFileSystem = uefi.protocol.SimpleFileSystem;
+const Allocator = std.mem.Allocator;
 
 pub fn main() void {
-    @panic("oh no! a bootloader!");
+    load() catch |err| @panic(@errorName(err));
+    @panic("loader returned unexpectedly");
+}
+
+fn load() !void {
+    const boot = uefi.system_table.boot_services.?;
+    const disk = (try boot.locateProtocol(SimpleFileSystem, null)).?;
+    const esp = try disk.openVolume();
+    const kernel_path = unicode.utf8ToUtf16LeStringLiteral("\\kernel.bin");
+    const kernel_file = try esp.open(kernel_path, .read, .{ .read_only = true });
+
+    _ = kernel_file;
+
+    return error.KernelFound;
 }
