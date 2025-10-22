a2bab5b491f8c27fdf4451ea2b628549c0f6cd63
Richard Remer <rich.remer@gmail.com>
2025-10-21 22:26:16 -0700

    better error handling

--- bootz64.efi/src/bootx64.zig
+++ bootz64.efi/src/bootx64.zig
@@ -5,6 +5,20 @@ const uefi = std.os.uefi;
 const SimpleFileSystem = uefi.protocol.SimpleFileSystem;
 const File = uefi.protocol.File;
 
+pub const BootError = error{
+    ELFHeader,
+    ELFLoadSegment,
+    ELFNoCode,
+    ELFProgramHeaders,
+    KernelFound,
+    LocateProtocol,
+    MemoryMapInfo,
+    MemoryMap,
+    OpenKernel,
+    OpenVolume,
+    OutOfMemory,
+};
+
 pub fn main() void {
     load() catch |err| @panic(@errorName(err));
     @panic("loader returned unexpectedly");
@@ -18,12 +32,12 @@ fn load() !void {
 
     const allocator = uefi.pool_allocator;
     const boot = uefi.system_table.boot_services.?;
-    const mmap_info = try boot.getMemoryMapInfo();
+    const mmap_info = boot.getMemoryMapInfo() catch return BootError.MemoryMapInfo;
     const mmap_size = mmap_info.descriptor_size * mmap_info.len;
     const mmap_buffer = try allocator.alloc(u8, mmap_size);
     defer allocator.free(mmap_buffer);
 
-    const mmap = try boot.getMemoryMap(@alignCast(mmap_buffer));
+    const mmap = boot.getMemoryMap(@alignCast(mmap_buffer)) catch return BootError.MemoryMap;
 
     // find highest physical address in map to place kernel
     var mmap_it = mmap.iterator();
@@ -34,15 +48,16 @@ fn load() !void {
         }
     }
 
-    const disk = (try boot.locateProtocol(SimpleFileSystem, null)).?;
-    const esp = try disk.openVolume();
+    const protocol = boot.locateProtocol(SimpleFileSystem, null) catch return BootError.LocateProtocol;
+    const disk = protocol.?;
+    const esp = disk.openVolume() catch return BootError.OpenVolume;
     defer esp.close() catch {};
 
     const kernel_path = unicode.utf8ToUtf16LeStringLiteral("\\kernel.bin");
-    const kernel_file = try esp.open(kernel_path, .read, .{ .read_only = true });
+    const kernel_file = esp.open(kernel_path, .read, .{ .read_only = true }) catch return BootError.OpenKernel;
     defer kernel_file.close() catch {};
 
-    const elf_header = try read_elf_header(kernel_file);
+    const elf_header = read_elf_header(kernel_file) catch return BootError.ELFHeader;
     const prog_headers_size = elf_header.phnum * elf_header.phentsize;
     const headers_size = elf_header.phoff + prog_headers_size;
     const headers_buffer = try allocator.alloc(u8, headers_size);
@@ -53,7 +68,7 @@ fn load() !void {
 
     var ph_it = elf_header.iterateProgramHeadersBuffer(headers_buffer);
     var first_segment = true;
-    while (try ph_it.next()) |segment| {
+    while (ph_it.next() catch return BootError.ELFProgramHeaders) |segment| {
         if (segment.p_type == elf.PT_LOAD) {
             // assume first loaded segment is kernel
             if (first_segment) {
@@ -62,21 +77,21 @@ fn load() !void {
                 first_segment = false;
             }
 
-            try load_segment(
+            load_segment(
                 kernel_file,
                 segment.p_offset,
                 segment.p_filesz,
                 segment.p_memsz,
                 segment.p_vaddr - vaddr_offset,
-            );
+            ) catch return BootError.ELFLoadSegment;
         }
     }
 
     if (first_segment) {
-        return error.NoCode;
+        return BootError.ELFNoCode;
     }
 
-    return error.KernelFound;
+    return BootError.KernelFound;
 }
 
 fn load_segment(
