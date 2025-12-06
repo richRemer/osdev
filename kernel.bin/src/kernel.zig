58cbcfa48f670823ce87619526a112543448f66a
Richard Remer <rich.remer@gmail.com>
2025-12-05 16:20:08 -0800

    setup kernel stack

--- kernel.bin/src/kernel.zig
+++ kernel.bin/src/kernel.zig
@@ -1,9 +1,27 @@
 const uart = @import("uart.zig");
 const ansi = @import("ansi.zig");
 
-export fn kmain() void {
+// https://wiki.osdev.org/Zig_Bare_Bones
+
+var stack: [0x4000]u8 align(16) linksection(".bss") = undefined;
+
+export fn kenter() callconv(.naked) noreturn {
+    asm volatile (
+        \\ movl %[stack_ptr], %%esp
+        \\ movl %%esp, %%ebp
+        \\ call %[kmain:P]
+        :
+        : [stack_ptr] "i" (&@as([*]align(16) u8, @ptrCast(&stack))[stack.len]),
+          [kmain] "X" (&kmain),
+    );
+}
+
+noinline fn kmain() callconv(.c) noreturn {
     uart.initialize();
     uart.puts(ansi.clrscr);
     uart.puts("kernel has taken over\n");
-    while (true) {}
+
+    while (true) {
+        asm volatile ("hlt");
+    }
 }
